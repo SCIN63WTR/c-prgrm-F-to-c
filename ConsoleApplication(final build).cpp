adaf2c7b0b1269a139bc18416ddc7459e93bc20c
SCIN63WTR <84226315+SCIN63WTR@users.noreply.github.com>
2021-05-23 02:27:35 +0530

    Add files via upload

---
+++ ConsoleApplication(final build).cpp
@@ -0,0 +1,30 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float farenheit, celcius;
+	cout << "Temperature Convertor :\n";
+	cout << " Input the temperature in Celsius : ";
+	cin >> celcius;
+	farenheit = (celcius * 9.0) / 5.0 + 32;
+	cout << " The temperature in Celsius    : " << celcius << endl;
+	cout << " The temperature in Fahrenheit : " << farenheit << endl;
+	cout << endl;
+	int x;
+	cin >> x;
+return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
